1612ca00c267880b29a35b4413444dfd4729888d
Anakin <835291568@qq.com>
2023-07-21 16:23:03 +0000

    新增无锁队列实现

--- main.c
+++ main.c
@@ -1,44 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdatomic.h>
-#define QUEUE_SIZE 100
-typedef struct
-{
-    int data[QUEUE_SIZE];
-    atomic_int head;
-    atomic_int tail;
-} LockFreeQueue;
+#include "lib/LockFreeQueue.h"
 
-void enqueue(LockFreeQueue *queue, int item)
-{
-    int tail = atomic_load(&queue->tail);
-    int nextTail = (tail + 1) % QUEUE_SIZE;
-    if (nextTail != atomic_load(&queue->head))
-    {
-        queue->data[tail] = item;
-        atomic_store(&queue->tail, nextTail);
-    }
-    else
-    {
-        printf("Queue is full. Unable to enqueue item %d\n", item);
-    }
-}
-
-int dequeue(LockFreeQueue *queue)
-{
-    int head = atomic_load(&queue->head);
-    if (head != atomic_load(&queue->tail))
-    {
-        int item = queue->data[head];
-        atomic_store(&queue->head, (head + 1) % QUEUE_SIZE);
-        return item;
-    }
-    else
-    {
-        printf("Queue is empty. Unable to dequeue item.\n");
-        return -1; // or any other value to indicate failure
-    }
-}
 int main()
 {
     LockFreeQueue queue;
